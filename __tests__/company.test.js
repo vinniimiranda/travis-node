5cfca0f708077a31be3d73de5130bd620272c97e
Flavio Miranda <vinniimiranda@gmail.com>
2019-10-30 16:31:10 -0300

    Initial commit

---
+++ __tests__/company.test.js
@@ -0,0 +1,7 @@
+describe('Company', () => {
+  it('shoud create an company', () => {
+    const sum = (a, b) => a + b;
+
+    expect(sum(1, 1)).toBe(2);
+  });
+});
---
+++ __tests__/user.test.js
@@ -0,0 +1,7 @@
+describe('User', () => {
+  it('shoud return an user', () => {
+    const sum = (a, b) => a + b;
+
+    expect(sum(1, 1)).toBe(2);
+  });
+});
